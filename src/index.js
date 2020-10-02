08aeba43fb5c8fdca70dbf8aef55d87b6beac6eb
shadowinhaze <shadowinhaze@gmail.com>
2020-10-02 19:36:23 +0300

    init: start and complete basic arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
+  if (typeof array !== 'undefined' && array.length >= 1) return Math.min(...array);
   return 0;
 }
 
 exports.max = function max (array) {
+  if (typeof array !== 'undefined' && array.length >= 1) return Math.max(...array);
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (typeof array !== 'undefined' && array.length >= 1) return array.reduce((prev,current) => prev + current, 0) / array.length;
   return 0;
 }
